7159de424fe9de4f54b682cec08a34f43337e5e5
regarys <yahyaseptian26@gmail.com>
2025-06-22 18:11:34 +0700

    update css html admin-dashboard

--- src/page/dashboard-admin/DashboardAdmin.jsx
+++ src/page/dashboard-admin/DashboardAdmin.jsx
@@ -1,14 +1,16 @@
-
+import { useState } from 'react';
 
 function DashboardAdmin() {
+  const [search, setSearch ] = useState('');
+
   return(
      <>
       <div className="dashboard-admin">
         <header>
           <h1>Kesra</h1>
           <form>
-            <input type="text" name="" value="search"/>
-            <button type="submit">Go</button>
+            <input type="text" name="" placeholder="SEARCH..."/>
+            <button type="submit">GO</button>
           </form>
           <a href="#" className="notification">
             <img src="/notification.png" alt="notifikasi icon"/>
@@ -18,7 +20,12 @@ function DashboardAdmin() {
         <div className="container-admin">
           <div className="sidebar-admin">
             <div className="sidebar-admin-profile">
-              <img src="/profile.jpg" alt="profile picure"/>
+              <img src="/profile.jpg" alt="profile picture"/>
+              <div className="profile-desc">
+                <h3>Regarys</h3>
+                <p>regarys@gmail.com</p>
+                <p>* ONLINE</p>
+              </div>
             </div>
             <div className="sidebar-admin-navigation">
               <p>check</p>
@@ -26,19 +33,34 @@ function DashboardAdmin() {
           </div>
           <div className="main-admin">
             <div className="file-buttons">
-              <p>LPJ</p>
-              <p>LPJ</p>
-              <p>LPJ</p>
+              <div className="proposal">
+                <img src="/propsal.jpg" alt="proposal"/>
+                <div className="overlay blue"></div>
+                <p>PROPOSAL</p>
+              </div>
+              <div className="proposal">
+                <img src="/lpj.jpg" alt="lpj"/>
+                <div className="overlay yellow"></div>
+                <p>LPJ</p>
+              </div>
+              <div className="proposal">
+                <img src="/sm.jpg" alt="surat masuk"/>
+                <div className="overlay orange"></div>
+                <p>LPJ</p>
+              </div>
             </div>
             <div className="container-info-admin">
               <div className="persentage-admin">
-                <p><span className="progress-value-admin">50%</span></p>
-                <p><span className="progress-value-admin">70%</span></p>
-                <p><span className="progress-value-admin">100%</span></p>
+                <p><span className="progress-value-admin proposal-p">50%</span></p>
+                <p><span className="progress-value-admin lpj-p">70%</span></p>
+                <p><span className="progress-value-admin sm-p">100%</span></p>
               </div>
               <div className="calendar-admin">
-                
+                <p>hola</p>
               </div>
+            </div>
+            <div className="note-admin">
+              <p>Catatan Terkini</p>
             </div>
           </div>
         </div>
